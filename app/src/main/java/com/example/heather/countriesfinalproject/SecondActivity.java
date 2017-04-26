339721388886259a7393ea061760433c93003c70
hcarlson1 <hcarlson1@css.edu>
2017-04-26 11:45:12 -0500

    Gibbons said redo second activity

--- app/src/main/java/com/example/heather/countriesfinalproject/SecondActivity.java
+++ app/src/main/java/com/example/heather/countriesfinalproject/SecondActivity.java
@@ -17,6 +17,7 @@ public class SecondActivity extends AppCompatActivity {
     TextView tvName, tvCapital, tvAltSPelling, tvRegion, tvPopulation, tvTimeZone, tvFlag;
     Button buttonBack;
     Country countryData;
+    String countryToGet;
 
 
     protected void onCreate(Bundle savedInstanceState) {
